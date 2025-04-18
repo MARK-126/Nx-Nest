00eb304a1d070e3536a6357959bb0843a90fbef4
Marcos <marcosrio.26@gmail.com>
2025-04-17 22:46:42 -0300

    Add user model and module

---
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma-clients/jobber-auth'
+import { PrismaService } from '../prisma/prisma.service'
+import { hash } from 'bcryptjs'
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10),
+      },
+    })
+  }
+
+  async findAll() {
+    return this.prismaService.user.findMany()
+  }
+}
