3d1c69205af4f8cd3abf826b260fce6f91f150d8
Marcos <marcosrio.26@gmail.com>
2025-04-17 22:47:06 -0300

    Add abstract module

---
+++ libs/nestjs/src/lib/graphql/abstract.module.ts
@@ -0,0 +1,11 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+
+@ObjectType({ isAbstract: true })
+export class AbstractModel {
+  @Field(() => ID)
+  id: number
+
+  constructor(id: number) {
+    this.id = id
+  }
+}
